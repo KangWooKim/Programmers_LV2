e3864adabaa843caba9bfd76a84d49f587ad505f
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-06-05 20:01:06 +0900

    Create 위장.cpp

---
+++ 위장.cpp
@@ -0,0 +1,19 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42578
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> changes;
+    for(const auto& i : clothes){
+        changes[i[1]]++;
+    }
+    for(const auto& i : changes){
+        
+        answer *= (i.second + 1);
+    }
+    answer--;
+    return answer;
+}
