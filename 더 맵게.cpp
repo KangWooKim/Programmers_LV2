0b470188dc0f54a53d1bf0e69c9b17a7bb784817
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-07-31 13:19:38 +0900

    Update 더 맵게.cpp

--- 더 맵게.cpp
+++ 더 맵게.cpp
@@ -1,1 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42626
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> q;
+    for(int i = 0 ; i < scoville.size() ; i++){
+        q.push(scoville[i]);
+    }
+    while(q.top() < K && q.size() != 1){
+        int i1 = q.top(); q.pop();
+        int i2 = q.top(); q.pop();
+        q.push(i1 + i2 * 2);
+        answer++;
+    }
+    if(q.size() == 1 && q.top() < K) answer = -1;
+    return answer;
+}
